274483d5c9448d3b332f562bbb02747491283cea
Victor Johan <Vicjohan23@gmail.com>
2020-05-28 22:29:08 -0400

    SE CREARON LAS CARPETAS: DAL, DATAY ENTIDADES.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPrestamo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroPrestamo.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= DATA\RegistroPrestamo.db");
+        }
+    }
+}
